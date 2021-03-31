14c214db741ac4b0d6d6fd82563cf3a7ad69959a
Carlos Roberto <carlosmedeiroslima@gmail.com>
2021-03-31 10:03:21 -0300

    Implementando o Map no lugar do forEach

--- map/01-Função_Map.js
+++ map/01-Função_Map.js
@@ -20,3 +20,27 @@ console.log("Usando Map");
 
 //https://www.youtube.com/watch?v=DFy0T-pWwLo&list=PLhxF6V44XvXQzeZqn3Xd95RKm_Gr7JT7y&index=2
 //Continuar de 02:49
+
+//PROBLEMA APONTADO: Pegar o array do mapData.js e gerar uma nova projeção mostrando objetos
+//apenas com id e titulo
+
+let videosComIdETitulo = [];
+//Usando o forEach para Solução
+console.log("Usando forEach para Projeção de videos e títulos");
+videos.forEach(video =>{
+    videosComIdETitulo.push({id : video.id, title : video.title})
+});
+console.log(videosComIdETitulo);
+
+//Usando código mais declarativo com Map
+console.log("Usando Map Como código mais declarativo");
+videosComIdETitulo = [];
+videosComIdETitulo = videos.map(video =>({id : video.id, title: video.title}));
+
+console.log(videosComIdETitulo);
+
+
+
+
+
+
